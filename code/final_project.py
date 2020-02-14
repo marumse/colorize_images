502bdcd4fad4f857f988d7b9074ea4821b78f6cd
unknown <sophia.schulze-weddige@gmx.de>
2020-02-14 21:50:08 +0100

    blabla

--- code/final_project.py
+++ code/final_project.py
@@ -46,6 +46,8 @@ if __name__ == "__main__":
     # compile model
     model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
     # fit model
+    from PIL import ImageFile
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
     model.fit_generator(train_it, steps_per_epoch=16, validation_data=val_it, validation_steps=8)
 
     # evaluate model
