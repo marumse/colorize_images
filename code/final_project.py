8979a1c7b4e171f5a5a57e18eba822dfc958bb55
unknown <sophia.schulze-weddige@gmx.de>
2020-02-15 17:47:11 +0100

    current version with self made generator

--- code/final_project.py
+++ code/final_project.py
@@ -2,14 +2,18 @@ import numpy as np
 import tensorflow as tf 
 #from skimage import color
 import cv2
+import os
+import matplotlib.pyplot as plt
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 from tensorflow.keras.models import Sequential
 
-from tensorflow.keras.layers import Conv2D, Activation, BatchNormalization, Softmax
+from tensorflow.keras.layers import Conv2D, Activation, BatchNormalization, Softmax, Multiply
 
 from tensorflow.keras.optimizers import Adam
+
+
 
 
 def transform_image(img):
@@ -31,8 +35,33 @@ def generate_dataset(path_to_train, path_to_val, path_to_test, path_to_save, bat
     # change color space to lab
     return datagen, train_it, val_it, test_it
 
+def generate_data(directory, batch_size):
+    """Replaces Keras' native ImageDataGenerator from: https://stackoverflow.com/questions/46493419/use-a-generator-for-keras-model-fit-generator"""
+    i = 0
+    file_list = os.listdir(directory)
+    while True:
+        image_batch = []
+        label_batch = []
+        for b in range(batch_size):
+            if i == len(file_list):
+                i = 0
+                random.shuffle(file_list)
+            sample = file_list[i]
+            i += 1
+            #image = cv2.resize(cv2.imread(sample[0]), (224,224))
+            image = cv2.resize(cv2.imread(directory + sample), (224,224))
+            L = image[:,:,0]
+            L = L[:,:,np.newaxis]
+            ab = image[:,:,1:]
+            image_batch.append(L)
+            label_batch.append(ab)
+
+        yield (np.array(image_batch), np.array(label_batch))
+
 
 if __name__ == "__main__":
+
+    
 
     #from PIL import ImageFile
     #ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -41,35 +70,36 @@ if __name__ == "__main__":
     #path_to_test = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/LabelFiles/colorize_images/test/'
     #path_to_train = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/LabelFiles/colorize_images/train/'
     #path_to_val = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/LabelFiles/colorize_images/validation/'
-    path_to_train = 'C:/Users/Acer/colorize_images/ex_pics/train/'
+    path_to_train = 'C:/Users/Acer/colorize_images/ex_pics/train/train/'
+    path_to_val = 'C:/Users/Acer/colorize_images/ex_pics/validation/validation/'
     batch_size = 3
 
-    # data generator for large datasets
-    # featurewise_center=True, featurewise_std_normalization=True)
-    # if we want this we need .fit()
-    datagen = ImageDataGenerator(preprocessing_function = transform_image) 
-    # load and iterate training dataset
-    train_it = datagen.flow_from_directory(path_to_train, target_size=(224,224), class_mode=None, batch_size=batch_size) # for class_mode=None we need subfolders in dir?
-    # load and iterate validation dataset
-    val_it = datagen.flow_from_directory(path_to_train, target_size=(224,224), class_mode=None, batch_size=batch_size)
-    # load and iterate test dataset
-    #test_it = datagen.flow_from_directory(path_to_test, target_size=(224,224), class_mode=None, batch_size=batch_size)
-    # confirm the iterator works
-    batchX = train_it.next()
-    print('Batch shape=%s, min=%.3f, max=%.3f' % (batchX.shape, batchX.min(), batchX.max()))
-    L, a, b = tf.unstack(batchX, axis = 3)
-    #print(L.shape) # (32,224,224)
-    #print(a.shape) # (32,224,224)
-    #print(b.shape) # (32,224,224)
-    ab = tf.stack([a,b], axis = -1)
-    #print(ab.shape) # (32,224,224,2)
-    L = tf.expand_dims(L, axis = 3)
-    #print(L.shape) # (32,224,224,1)
+    # # data generator for large datasets
+    # # featurewise_center=True, featurewise_std_normalization=True)
+    # # if we want this we need .fit()
+    # datagen = ImageDataGenerator(preprocessing_function = transform_image) 
+    # # load and iterate training dataset
+    # train_it = datagen.flow_from_directory(path_to_train, target_size=(224,224), class_mode=None, batch_size=batch_size) # for class_mode=None we need subfolders in dir?
+    # # load and iterate validation dataset
+    # val_it = datagen.flow_from_directory(path_to_train, target_size=(224,224), class_mode=None, batch_size=batch_size)
+    # # load and iterate test dataset
+    # #test_it = datagen.flow_from_directory(path_to_test, target_size=(224,224), class_mode=None, batch_size=batch_size)
+    # # confirm the iterator works
+    # batchX = train_it.next()
+    # print('Batch shape=%s, min=%.3f, max=%.3f' % (batchX.shape, batchX.min(), batchX.max()))
+    # L, a, b = tf.unstack(batchX, axis = 3)
+    # #print(L.shape) # (32,224,224)
+    # #print(a.shape) # (32,224,224)
+    # #print(b.shape) # (32,224,224)
+    # ab = tf.stack([a,b], axis = -1)
+    # #print(ab.shape) # (32,224,224,2)
+    # L = tf.expand_dims(L, axis = 3)
+    # #print(L.shape) # (32,224,224,1)
     
     # define model
     model = Sequential()
     # conv1
-    model.add(Conv2D(64, (3,3), padding='same', input_shape=L.shape[1:]))
+    model.add(Conv2D(64, (3,3), padding='same', input_shape=(224,224,1)))
     model.add(Activation('relu'))
     model.add(Conv2D(64, (3,3), strides = 2, padding='same'))
     model.add(Activation('relu'))
@@ -130,7 +160,7 @@ if __name__ == "__main__":
 
     # softmax layer
     model.add(Conv2D(313, (1,1), strides = 1, dilation_rate = 1))
-    #model.add(Multiply())
+    model.add(Multiply())
     model.add(Softmax())
 
     # decoding layer
@@ -141,7 +171,7 @@ if __name__ == "__main__":
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
     print(model.summary())
     # fit model
-    model.fit_generator(train_it, steps_per_epoch=3, validation_data=val_it, validation_steps=8)
+    model.fit_generator(generate_data(path_to_train, batch_size), steps_per_epoch=3, validation_data=generate_data(path_to_val,batch_size), validation_steps=8)
 
 
     # save weights
