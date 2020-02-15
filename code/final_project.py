bf9a4f567fc3a54d935878e35bf96b8f98c2124e
marumse <mborn@uos.de>
2020-02-15 15:29:18 +0100

    pictures for debugging

--- code/final_project.py
+++ code/final_project.py
@@ -1,5 +1,5 @@
-import numpy as np 
-import tensorflow as tf 
+import numpy as np
+import tensorflow as tf
 #from skimage import color
 import cv2
 
@@ -20,14 +20,14 @@ def transform_image(img):
 def generate_dataset(path_to_train, path_to_val, path_to_test, path_to_save, batch_size=3):
     # featurewise_center=True, featurewise_std_normalization=True)
     # if we want this we need .fit()
-    datagen = ImageDataGenerator(preprocessing_function = transform_image) 
+    datagen = ImageDataGenerator(preprocessing_function = transform_image)
     # load and iterate training dataset
     train_it = datagen.flow_from_directory(path_to_train, target_size=(224,224), save_to_dir = path_to_save, class_mode=None, batch_size=batch_size) # for class_mode=None we need subfolders in dir?
     # load and iterate validation dataset
     val_it = datagen.flow_from_directory(path_to_val, target_size=(224,224), class_mode=None, batch_size=batch_size)
     # load and iterate test dataset
     test_it = datagen.flow_from_directory(path_to_test, target_size=(224,224), class_mode=None, batch_size=batch_size)
-    
+
     # change color space to lab
     return datagen, train_it, val_it, test_it
 
@@ -37,16 +37,16 @@ if __name__ == "__main__":
     from PIL import ImageFile
     ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-    path_to_save = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/LabelFiles/colorize_images/save/'
-    path_to_test = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/LabelFiles/colorize_images/test/'
-    path_to_train = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/LabelFiles/colorize_images/train/'
-    path_to_val = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/LabelFiles/colorize_images/validation/'
+    path_to_save = 'C:/Users/schmuri/Desktop/testbilder/save/'
+    path_to_test = 'C:/Users/schmuri/Desktop/testbilder/test/'
+    path_to_train = 'C:/Users/schmuri/Desktop/testbilder/train/'
+    path_to_val = 'C:/Users/schmuri/Desktop/testbilder/validation/'
     batch_size = 3
 
     # data generator for large datasets
     # featurewise_center=True, featurewise_std_normalization=True)
     # if we want this we need .fit()
-    datagen = ImageDataGenerator(preprocessing_function = transform_image) 
+    datagen = ImageDataGenerator(preprocessing_function = transform_image)
     # load and iterate training dataset
     train_it = datagen.flow_from_directory(path_to_train, target_size=(224,224), save_to_dir = path_to_save, class_mode=None, batch_size=batch_size) # for class_mode=None we need subfolders in dir?
     # load and iterate validation dataset
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     #print(ab.shape) # (32,224,224,2)
     L = tf.expand_dims(L, axis = 3)
     #print(L.shape) # (32,224,224,1)
-    
+
     # define model
     model = Sequential()
     # conv1
@@ -134,7 +134,7 @@ if __name__ == "__main__":
 
     # decoding layer
     model.add(Conv2D(2, (1,1), stride = 1, dilation_rate = 1))
-    
+
     # compile model
     model.compile(optimizer=Adam(0.01))
     # fit model
