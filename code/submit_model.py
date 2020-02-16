c6f43d723468a6ac0c05c18ff7a28e6565845444
marumse <mborn@uos.de>
2020-02-16 15:50:22 +0100

    typo

--- code/submit_model.py
+++ code/submit_model.py
@@ -1,7 +1,7 @@
 from grid import*
 
 if __name__ == '__main__':
-    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/colorize_images/code/final_project.py'
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/colorize_images/code/final_project.py'
     path_to_train = '/net/projects/data/ImageNet/ILSVRC2012/train'
     path_to_val = '/net/projects/data/ImageNet/ILSVRC2012/val'
     batch_size = 25
