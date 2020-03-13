747f422c7d53aeb0d3607d84a9f9910344cab29a
unknown <sophia.schulze-weddige@gmx.de>
2020-03-13 14:12:27 +0100

    try to save image L

--- code/final_project.py
+++ code/final_project.py
@@ -44,12 +44,9 @@ def generate_data(directory, batch_size):
     """ Replaces Keras' native ImageDataGenerator.
         code snippet from: https://stackoverflow.com/questions/46493419/use-a-generator-for-keras-model-fit-generator
     """
-    print("check3.1")
     i = 0
     file_list = list_files(directory)
-    print("check3.2")
     while True:
-        print("check3.3")
         image_batch = []
         label_batch = []
         for b in range(batch_size):
@@ -64,17 +61,14 @@ def generate_data(directory, batch_size):
             image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
             L = image[:,:,0]
             L = L[:,:,np.newaxis]
-            print("checkpoint3.3.1")
-            try:
-                print(x)
-            except:
-                print("x not defined")
+            plt.imshow(L)
+            plt.savefig('/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/colorize_images/code/L.png')
             ab = image[:,:,1:]
-            print("3.3.2")
+            print("check ab")
             image_batch.append(L)
-            print("3.3.3")
+            print("check L append")
             label_batch.append(ab)
-            print("check3.4")
+            print("check ab appand")
         yield (np.array(image_batch), np.array(label_batch))
 
 
@@ -82,14 +76,12 @@ if __name__ == "__main__":
     #print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
     
     tf.debugging.set_log_device_placement(True)
-    print("checkpoint1")
     args = typecast(sys.argv[1:])
     path_to_train = args[0]
     path_to_val = args[1]
 
     batch_size = args[2]
 
-    print(tf.test.is_gpu_available())
     # define model
     model = Sequential()
     # conv1
@@ -160,20 +152,19 @@ if __name__ == "__main__":
     # decoding layer
     model.add(Conv2DTranspose(313, (4,4), strides = 16, padding = 'same'))
     model.add(Conv2D(2, (1,1), strides = 1, dilation_rate = 1))
-    print("checkpoint2")
+
     # compile model
     sgd = tf.keras.optimizers.SGD(lr=0.001, momentum=0.9, nesterov=True, clipnorm=5.)
     mse = tf.keras.losses.MeanSquaredError()
     model.compile(optimizer=sgd, loss=mse)
     #print(model.summary())
-    print("checkpoint3")
+
     # fit model
     history = model.fit_generator(generate_data(path_to_train, batch_size), steps_per_epoch=400, epochs=5, validation_data=generate_data(path_to_val,batch_size), validation_steps=8)
     print(history.history)
     # save weights
     model.save_weights('/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/colorize_images/code/first_try.h5')
     # evaluate model
-    print("checkpoint4")
     plt.figure(facecolor='white')
 
     plt.plot(history.history['loss'], label="loss", color="blue")
