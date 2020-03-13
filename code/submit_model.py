cf7e707026dbda163f65e24f1e3af7df77dc5625
unknown <sophia.schulze-weddige@gmx.de>
2020-03-13 18:24:15 +0100

    reduce batch size to 2

--- code/submit_model.py
+++ code/submit_model.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/colorize_images/code/final_project.py'
     path_to_train = '/net/projects/data/ImageNet/ILSVRC2012/train'
     path_to_val = '/net/projects/data/ImageNet/ILSVRC2012/val'
-    batch_size = 20
+    batch_size = 2
     args = [path_to_train, path_to_val, batch_size]
 
     env = 'source /net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate MultiLabel'
