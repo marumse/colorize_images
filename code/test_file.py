8f5aca2a41805098e3f04a7fdf112b9df754d864
unknown <sophia.schulze-weddige@gmx.de>
2020-03-13 14:19:29 +0100

    reduced script to only generate the dataset

--- code/test_file.py
+++ code/test_file.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/colorize_images/code/final_project.py'
     path_to_train = '/net/projects/data/ImageNet/ILSVRC2012/train'
     path_to_val = '/net/projects/data/ImageNet/ILSVRC2012/val'
-
+    batch_size = 2
     file_list = list_files(path_to_train)
     print(file_list)
     # fit model
