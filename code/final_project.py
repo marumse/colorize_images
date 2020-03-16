4d9c61b40b8a99545311c37b026ee402642a182d
unknown <sophia.schulze-weddige@gmx.de>
2020-03-16 14:41:13 +0100

    testing

--- code/final_project.py
+++ code/final_project.py
@@ -185,9 +185,13 @@ if __name__ == "__main__":
     history = model.fit_generator(train_gen, steps_per_epoch=10, epochs=1, validation_data=val_gen, validation_steps=1)
     print(history.history)
 
-    prediction = model.predict_generator(test_gen, steps=1, max_queue_size=10)
+    prediction = model.predict_generator(test_gen, steps=3, max_queue_size=10)
     test_data = np.array(train_gen)
     print(test_data.shape)
+    test_in = test_data[0,:,:,:]
+    print(test_in.shape)
+    test_out = test_data[1,:,:,:]
+
     print(prediction.shape)
     # save weights
     model.save_weights('/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/colorize_images/code/second_try.h5')
