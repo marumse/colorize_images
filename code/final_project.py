9f93b4415ef0c1994cad402e10e8af46489f392c
unknown <sophia.schulze-weddige@gmx.de>
2020-03-31 16:06:32 +0200

    test

--- code/final_project.py
+++ code/final_project.py
@@ -228,7 +228,7 @@ if __name__ == "__main__":
     #model.save_weights('/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/colorize_images/code/small_batch_few_epochs.h5')
     
     # make a prediction and save the image
-    make_prediction(val_files)
+    make_prediction(val_files[:-1])
 
     # plot and save the accuracy and loss values
     #plot_history(history)
