373e38c0badf05f8932bda04c3bc8f4a74f37d6d
unknown <sophia.schulze-weddige@gmx.de>
2020-04-16 13:22:51 +0200

    missing :

--- code/submit_model.py
+++ code/submit_model.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     env = 'source /net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate MultiLabel'
 
-    for lr, name in zip(learning_rates, names)
+    for lr, name in zip(learning_rates, names):
         args = [path_to_train, path_to_val, path_to_test, batch_size, name, mode, lr]
 
         submit_script(path, args, env)
