1ee1dc3d154666fd5f037c7329df1d805534d7ec
MalinSpaniol <mspaniol@uos.de>
2020-04-18 11:51:56 +0200

    further change to run on malins repo

--- code/submit_model.py
+++ code/submit_model.py
@@ -1,7 +1,7 @@
 from grid import*
 
 if __name__ == '__main__':
-    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/colorize_images/code/reduce_colors.py'
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/malin/colorize_images/code/reduce_colors.py'
     path_to_train = '/net/projects/data/ImageNet/ILSVRC2012/train'
     path_to_val = '/net/projects/data/ImageNet/ILSVRC2012/val'
     path_to_test = '/net/projects/data/ImageNet/ILSVRC2012/test'
